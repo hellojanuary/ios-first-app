9c9dff2758a4d2b7fa81bfd1cd573bf99c65bf1d
JessH <jhuang@classpass.com>
2016-01-18 09:53:26 -0500

    added first files

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var jess = 15
+var vinny = 5
+var jv = jess + vinny
+var jv2 = jess * vinny
+var jv3 = jess - vinny
+var jv4 = jess/vinny
+
+var a = 1; var b = 2; var c = 3
+let dog = "dog"; let cat = "cat" ;
+
+var decimal: Double
---
+++ nums.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var myAge = 18
+
+myAge = 19
+
+var yourAge: UInt = 2
